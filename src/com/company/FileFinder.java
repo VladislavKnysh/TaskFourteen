7272ec0a8fdef582e6940b84636097bffff01514
Vladislav Knysh <vladik.knysh@gmail.com>
2021-01-27 18:29:53 +0200

    Commit after corrections

--- src/com/company/FileFinder.java
+++ src/com/company/FileFinder.java
@@ -3,6 +3,11 @@ package com.company;
 import com.company.exception.FileWasFoundException;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 
 public class FileFinder {
@@ -10,94 +15,32 @@ public class FileFinder {
     private File directory;
     private File fileToFind;
 
+
     public FileFinder(File directory, String fileName) {
         this.fileName = fileName;
         this.directory = directory;
     }
 
     public File find() {
-        try {
-        //Step One
-        if (checkDirectory(directory)) {
-            findInDirectory(directory);
-            return fileToFind;
-        }
-        //Step Two
-        if (!checkDirectoryForMoreDirectories(directory)) {
-            return null;
-        } else {
-            File[] directories = findSubDirectories(directory);
-            for (File file : directories) {
-                if (Objects.nonNull(operateFile(file))) {
-                    return file;
-                }
-
-            }
-        }
-        return null;
-    }catch (FileWasFoundException fileWasFoundException){return fileToFind;}
+        return find(directory);
     }
 
-    //Step Three
-    private File operateFile(File file) throws FileWasFoundException {
-        try {
-            if (checkDirectory(file)) {
-                findInDirectory(file);
-                return fileToFind;
-            }
-            if (!checkDirectoryForMoreDirectories(file)) {
-                return null;
-            } else {
-                File[] directories = findSubDirectories(file);
-                for (File subFile : directories) {
-                    if (Objects.nonNull(operateFile(subFile))) {
-                        return subFile;
+    private File find(File directory) {
+        if (directory.isDirectory()) {
+            File[] directoryFiles = directory.listFiles();
+            if (directoryFiles != null) {
+                for (File file : directoryFiles) {
+                    if (file.isDirectory()) {
+                        find(file);
+                    } else {
+                        if (file.getName().equalsIgnoreCase(fileName)) {
+                            fileToFind= file;
+                        }
                     }
                 }
             }
-            return null;
-        } catch (NullPointerException e) {
-            return null;
         }
-
-    }
-
-
-    private boolean checkDirectory(File file) {
-
-        File[] files = file.listFiles((dir, name) -> name.equalsIgnoreCase(fileName));
-
-        if (files.length > 0) {
-            return true;
-        } else return false;
-
-    }
-
-    private void findInDirectory(File file) throws FileWasFoundException {
-        File[] files = file.listFiles((dir, name) -> name.equalsIgnoreCase(fileName));
-
-        if (files.length > 0) {
-            fileToFind = files[0]
-            ;
-            throw new FileWasFoundException();
-        }
-    }
-
-
-    private boolean checkDirectoryForMoreDirectories(File file) {
-
-        File[] files = file.listFiles((dir, name) -> !dir.isFile());
-
-        if (files.length > 0) {
-            return true;
-        } else return false;
-
-    }
-
-    private File[] findSubDirectories(File file) {
-        File[] files = file.listFiles((dir, name) -> dir.isDirectory());
-        return files;
-    }
+    return fileToFind;}
 
 
     public String getFileName() {
